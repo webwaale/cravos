695d2b94c123e1fb6a29e3e5e7ba1ad5262204fd
Aman Gupta <debugger.aman@gmail.com>
2020-08-26 13:03:57 +0530

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+  $("#owl-carousel-1").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -20,6 +29,34 @@ $(document).ready(function () {
       },
       1000: {
         items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+
+  $("#owl-carousel-2").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>",
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      700: {
+        items: 4,
+        nav: false,
+      },
+      1000: {
+        items: 5,
         nav: true,
         loop: false,
       },
